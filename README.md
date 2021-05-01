e9ccfb5c69384e1d7de6e772d89736fe92c62f35
abbas yazdanmehr <77411860+abbasyazdanmehr@users.noreply.github.com>
2021-05-01 11:32:28 +0430

    project rebuild.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# seller_app
+# enterigester
 
 A new Flutter application.
 
--- android/app/src/main/kotlin/com/example/seller_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.seller_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
