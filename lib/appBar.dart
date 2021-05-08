8637d2c30767336dc64f494d5dc14e69310ae86b
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-05-08 19:06:41 +0430

    حذف آیکون نقشه از اپ بار

--- lib/appBar.dart
+++ lib/appBar.dart
@@ -9,9 +9,7 @@ AppBar appBar(){
         fontSize: 30, fontWeight: FontWeight.bold,fontStyle: FontStyle.italic)),
     centerTitle: true,
     elevation: 10,
-    actions: [
-      IconButton(onPressed:(){},icon:Icon(Icons.map,color: Colors.black45))
-    ],
+
   );
 
 }
