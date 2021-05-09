2f59eacaf86277a99c9be2e3a169bb1cccf35953
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-05-09 19:21:24 +0430

    دکمه بک تولبار مشکی شد

--- lib/Objects/Coustomer.dart
+++ lib/Objects/Coustomer.dart
@@ -1,7 +1,7 @@
-import 'package:customer_app/Objects/Restaurant.dart';
+import 'Restaurant.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'Food.dart';
-import 'Restaurant.dart';
+
 
 
 class Customer{
--- lib/appBar.dart
+++ lib/appBar.dart
@@ -9,7 +9,8 @@ AppBar appBar(){
         fontSize: 30, fontWeight: FontWeight.bold,fontStyle: FontStyle.italic)),
     centerTitle: true,
     elevation: 10,
-
+  iconTheme: IconThemeData(color: Colors.black),
   );
+
 
 }
